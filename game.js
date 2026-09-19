83b2e056f0dce4489fe579759e835f7bb533c4bf
reverse-shion <kisaki.aiki@gmail.com>
2026-09-19 20:22:29 +0900

    Point official flow to ORB-synced Alenon build

--- game.js
+++ game.js
@@ -1643,7 +1643,7 @@
     // Star Gate Garden directly.
     if (!enteringFromLanding) {
       if (start) start.disabled = true;
-      location.href = "./alenon.html?from=title&build=2ebf3d31";
+      location.href = "./alenon.html?from=title&build=b0f32233";
       return;
     }
 
