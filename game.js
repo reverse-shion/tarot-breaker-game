d1206e12b792f4bedaef58c783f95a213c1f23d8
reverse-shion <kisaki.aiki@gmail.com>
2026-09-19 20:12:23 +0900

    Bust official Alenon page cache on title route

--- game.js
+++ game.js
@@ -1643,7 +1643,7 @@
     // Star Gate Garden directly.
     if (!enteringFromLanding) {
       if (start) start.disabled = true;
-      location.href = "./alenon.html?from=title";
+      location.href = "./alenon.html?from=title&build=8462bfec";
       return;
     }
 
